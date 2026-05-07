558391ccedcc1a2dbd3a0702e4fb3022f77fc385
Andrés Muñoz <andres2078@gmail.com>
2026-05-06 23:54:56 -0500

    feat: add examples of immutable array to-methods

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './style.css'
-import '../01-arrays/array-with.js'
+// import '../01-arrays/array-structured-clone'
+// import '../01-arrays/array-with'
+import '../01-arrays/array-to-methods'
 
 document.querySelector('#app').innerHTML = `
   <div>
